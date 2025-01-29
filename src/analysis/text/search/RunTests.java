dbd5fac5244c2d053c6f40b81a2195cdaab56983
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-01-29 11:56:38 +0100

    forgot to add

---
+++ src/analysis/text/search/RunTests.java
@@ -0,0 +1,11 @@
+package analysis.text.search;
+
+import org.junit.runner.RunWith;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
+
+@RunWith(RascalJUnitTestRunner.class)
+@RascalJUnitTestPrefix("analysis::text::search")
+public class RunTests {
+
+}
